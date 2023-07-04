1446918723352f2a3c1294089ab20361e6be5043
Mikel Mejia <mikel.mejia.306@my.csun.edu>
2023-07-04 05:04:03 -0700

    added more details

--- launch/rplidar.launch.py
+++ launch/rplidar.launch.py
@@ -11,7 +11,7 @@ def generate_launch_description():
             executable='rplidar_composition',
             output='screen',
             parameters=[{
-                'serial_port': '/dev/serial/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.2:1.0-port0',
+                'serial_port': '/dev/serial/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.2:1.0-port0', #this is the bottom right USB port when looking at the USB port openings
                 'frame_id': 'lidar_frame',
                 'angle_compensate': True,
                 'scan_mode': 'Standard'
